e238d816bd507bbf85346b83dc74a63a02775b49
Echo <lunariannova@gmail.com>
2026-04-16 23:33:15 -0400

    Secondary doors

--- Wayfinder-Door-Unlocker/Class1.cs
+++ Wayfinder-Door-Unlocker/Class1.cs
@@ -1,5 +1,7 @@
-﻿using HarmonyLib;
+﻿using Bang.StateMachines;
+using HarmonyLib;
 using Road.StateMachines;
+using System.Reflection;
 using System.Reflection.Emit;
 using Wayfinder.Core;
 
@@ -45,5 +47,37 @@ public static class NPCDoor_Patch
 
         if (!foundAndReplaced)
             LoaderCore.LogError("Could not find the NPCDoorStateMachine _openPending check.");
+    }
+}
+
+// ughhh going insane
+[HarmonyPatch]
+public static class AllDoorsOpen_Patch
+{
+    private static readonly FieldInfo ClosedField = AccessTools.Field(typeof(DoorStateMachine), "_closed");
+    private static readonly MethodInfo AddPassageMethod = AccessTools.Method(typeof(DoorStateMachine), "AddPassageWhenOpened");
+    private static readonly MethodInfo PlayAnimationsMethod = AccessTools.Method(typeof(DoorStateMachine), "PlayDoorAnimations");
+
+    static IEnumerable<MethodBase> TargetMethods()
+    {
+        yield return AccessTools.Method(typeof(DoorStateMachine), nameof(DoorStateMachine.Start));
+        yield return AccessTools.Method(typeof(DoorStateMachine), nameof(DoorStateMachine.Closed));
+        yield return AccessTools.Method(typeof(DoorStateMachine), nameof(DoorStateMachine.Close));
+    }
+
+    static bool Prefix(DoorStateMachine __instance, ref IEnumerator<Wait> __result)
+    {
+        __result = ForceOpenForever(__instance);
+        return false;
+    }
+
+    static IEnumerator<Wait> ForceOpenForever(DoorStateMachine instance)
+    {
+        ClosedField?.SetValue(instance, false);
+        AddPassageMethod?.Invoke(instance, null);
+        PlayAnimationsMethod?.Invoke(instance, new object[] { true, new string[] { "opened" } });
+
+        // Don't call original
+        yield return Wait.Stop;
     }
 }
